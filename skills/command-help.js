7e2239b68726a7729b5e504cb7034f2ce3efdc2a
Thomas Falkenberg <thomas.falkenberg@payback.net>
2019-01-28 09:32:30 +0100

    added some help

--- skills/command-help.js
+++ skills/command-help.js
@@ -5,8 +5,9 @@ module.exports = function (controller) {
 
     controller.hears(["help", "who"], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
+        text += "\n- " + bot.enrichCommand(message, "lock") + ": lock an environment. This will just create a file in my working directory with some meta-info. ";
+        text += "\n- " + bot.enrichCommand(message, "unlock") + ": TODO. To unlock, use lock on a locked environment.";
         bot.reply(message, text);
     });
 }
