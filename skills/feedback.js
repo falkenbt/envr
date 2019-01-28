072fd02f4bc7ed9d1b8f69cbe33a13682982de45
Thomas Falkenberg <thomas.falkenberg@payback.net>
2019-01-28 13:01:54 +0100

    added question for feedback

--- skills/feedback.js
+++ skills/feedback.js
@@ -2,13 +2,22 @@ module.exports = function (controller) {
     var masterId = 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS80MzY5MWExNi0xMjQ2LTQ0MWItODAzMi02NGNkNTE1MGIwZjE' // user ID of Thomas Falkenberg
 
     controller.hears(["feedback"], "direct_message,direct_mention", function (bot, message) {
-        bot.reply(message, "Thanks for the feedback. I will forward it to my master.");
+       
+        bot.startConversation(message,function(err,convo) {
 
-        bot.say(
-            {
-              text: 'Feeback received',
-              channel: 'Y2lzY29zcGFyazovL3VzL1JPT00vNDVmMGY1ZTAtMTUyYS0xMWU5LTg3MmUtZWJlMmZjODhiOTQ2' 
+            convo.ask('Feedback is always welcome. What is yours?', function(response, convo) {
+                convo.say('Thanks for the feedback. I will forward it to my master.');
+                convo.next();
+                bot.say(
+                    {
+                      text: 'Feeback from '+ message.user +' received: '+response.text,
+                      channel: 'Y2lzY29zcGFyazovL3VzL1JPT00vNDVmMGY1ZTAtMTUyYS0xMWU5LTg3MmUtZWJlMmZjODhiOTQ2' 
+                    }
+                  );
+              })
             }
-          );
+            
+        )
+        
     });
 }
