98c31063b7092a434584f4d1936ae51b2572c114
Thomas Falkenberg <thomas.falkenberg@payback.net>
2019-01-28 10:48:12 +0100

    added feedback skill

---
+++ skills/feedback.js
@@ -0,0 +1,14 @@
+module.exports = function (controller) {
+    var masterId = 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS80MzY5MWExNi0xMjQ2LTQ0MWItODAzMi02NGNkNTE1MGIwZjE' // user ID of Thomas Falkenberg
+    
+    controller.hears(["lock"], "direct_message,direct_mention", function (bot, message) {
+        bot.reply(message, "Thanks for the feedback. I will forward it to my master.");
+
+        bot.say(
+            {
+              text: 'Feeback from ' + message.user + ': '+ message.text,
+              channel: masterId 
+            }
+          );
+    });
+}
