262632fb84a9dae1e0dc5d4bc6176d69fa86d1a9
Thomas Falkenberg <thomas.falkenberg@payback.net>
2019-01-28 09:45:01 +0100

    better help texts

--- skills/command-help.js
+++ skills/command-help.js
@@ -7,7 +7,7 @@ module.exports = function (controller) {
         var text = "Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "lock") + ": lock an environment. This will just create a file in my working directory with some meta-info. ";
-        text += "\n- " + bot.enrichCommand(message, "unlock") + ": TODO. To unlock, use lock on a locked environment.";
+        text += "\n- " + bot.enrichCommand(message, "unlock") + ": TODO. Fow now use 'lock' command to unlock.";
         bot.reply(message, text);
     });
 }
